6c0b20de5ffa17c58766ea4fa15f7932075508f5
vwegbadebekeme <vwegbadebekeme5@gmail.com>
2025-08-13 23:25:41 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  // amalvin~8y0xTLqQ#tO48A5ff4_3XFhKAmkOJ2_CkBbOB4Id4dGMmOX8P_owdd your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348086275816", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
